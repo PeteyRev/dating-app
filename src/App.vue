1e4f22435b8791df73326a16f9e02b5cc36e91b5
Peter Arevalo <pete.g.arevalo@gmail.com>
2018-02-13 00:41:51 -0600

    first commmit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header>
+      <button class="mobile-toggle"><i class="fas fa-bars"></i></button>
+      <nav>
+        <ul class="main-nav">
+          <li>
+            <router-link to="/" class="button"
+            active-class="active">link 1</router-link>
+          </li>
+          <li>
+            <router-link to="/" class="button"
+            active-class="active">link 2</router-link>
+          </li>
+          <li>
+            <router-link to="/" class="button"
+            active-class="active">link 2</router-link>
+          </li>
+        </ul>
+        </nav>
+    </header>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500i,700');
+@import "~foundation-sites/scss/foundation";
+@import "/scss/app";
+</style>
